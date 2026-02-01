a5a90a117d248ddc229cf97c20a9e28b0c4acca7
ChiaChun <chloechen97.ca@gmail.com>
2026-01-31 23:38:21 -0500

    Add a button that goes to the About Me

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
     <div className="page">
       <h2>Welcome</h2>
+
       <p>
         Hi, I’m Chia Chun Chen, also known as Chloe. This is my portfolio website built with React.
       </p>
@@ -15,6 +18,12 @@ export default function Home() {
         Feel free to explore my projects and services.
       </p>
 
+      {/* go to about */}
+      <div className="homeActions">
+        <Link to="/about" className="btnPrimary">
+          About Me
+        </Link>
+      </div>
     </div>
   );
 }
